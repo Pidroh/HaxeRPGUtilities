ca53011cae247667a607b230a0fbf6f94729cc8e
Pedro <forworksfr@hotmail.com>
2021-09-10 09:06:05 +0900

    actor get XP and levelling up after defeating enemy

--- BattleManager.hx
+++ BattleManager.hx
@@ -9,28 +9,41 @@ class BattleManager {
     var timePeriod:Float = 1;
 
     public function new (){
-        hero = {level:1, attributesBase:["Attack"=> 5, "Life" => 20], equipmentSlots: null, equipment: null};
-		enemy = {level:1, attributesBase:["Attack"=> 2, "Life" => 6], equipmentSlots: null, equipment: null};
+		
+		var stats = ["Attack"=> 5, "Life" => 20, "LifeMax" => 20];
+        hero = {level:1, attributesBase:stats, equipmentSlots: null, equipment: null, 
+			xp:ResourceLogic.getExponentialResource(1.15, 5, 5), attributesCalculated: stats};
+		var stats2 = ["Attack"=> 2, "Life" => 6];
+		enemy = {level:1, attributesBase:stats2, equipmentSlots: null, equipment: null, xp:null, attributesCalculated: stats2};
         timeCount = 0;
     }
 
 	public function advance() {
         var output : String = "";
-		if (hero.attributesBase["Life"] <= 0) {
+		if (hero.attributesCalculated["Life"] <= 0) {
             output += "You died\n\n\n";
-			hero.attributesBase["Life"] = 20;
-			enemy.attributesBase["Life"] = 6;
+			hero.attributesCalculated["Life"] = hero.attributesCalculated["LifeMax"];
+			enemy.attributesCalculated["Life"] = 6;
 			// c = Sys.getChar(true);
 		}
-		var herolife = hero.attributesBase["Life"];
-		if (enemy.attributesBase["Life"] <= 0) {
+		var herolife = hero.attributesCalculated["Life"];
+		if (enemy.attributesCalculated["Life"] <= 0) {
+			hero.xp.value += 2;
+			if(hero.xp.value > hero.xp.calculatedMax){
+				hero.xp.value = 0;
+				hero.level++;
+				AttributeLogic.Add(hero.attributesBase, ["Attack"=> 1, "LifeMax" => 1, "Life"=>1], hero.level, hero.attributesCalculated);
+				ResourceLogic.recalculateScalingResource(hero.level, hero.xp);
+			}
             output += "New enemy";
             output += "\n\n\n";
-			enemy.attributesBase["Life"] = 6;
+			enemy.attributesCalculated["Life"] = 6;
 			// c = Sys.getChar(true);
 		}
-		var enemylife = enemy.attributesBase["Life"];
-        output += 'Player life: $herolife';
+		var enemylife = enemy.attributesCalculated["Life"];
+		var xp = hero.xp.value;
+		var xpmax = hero.xp.calculatedMax;
+        output += 'Player life: $herolife xp: $xp xpmax:$xpmax';
         output += "\n";
         output += 'Enemy life: $enemylife';
         output += "\n";
@@ -41,7 +54,7 @@ class BattleManager {
 			attacker = enemy;
 			defender = hero;
 		}
-		defender.attributesBase["Life"] -= attacker.attributesBase["Attack"];
+		defender.attributesCalculated["Life"] -= attacker.attributesCalculated["Attack"];
 		turn = !turn;
         return output;
 	}
--- Main.hx
+++ Main.hx
@@ -101,14 +101,53 @@ class Main {
 			turn = !turn;
 		}
 	}
+}
 
-	public function Loop() {}
+class ResourceLogic {
+	public static function recalculateScalingResource(base:Int, res:ScalingResource) {
+		if (res.lastUsedBaseAttribute != base) {
+			var data1 = res.scaling.data1;
+			var calculated = Std.int(Math.pow(data1, base));
+
+			// uses only the minimum increment
+			calculated = calculated - calculated % res.scaling.minimumIncrement;
+			res.calculatedMax = calculated;
+			res.lastUsedBaseAttribute = base;
+			trace(res);
+		}
+	}
+
+	public static function getExponentialResource(expBase:Float, minimumIncrement:Int, initial:Int):ScalingResource {
+		var res = {
+			scaling: {data1: expBase, minimumIncrement: minimumIncrement, type: exponential},
+			value: 0,
+			lastUsedBaseAttribute: 0,
+			calculatedMax: 0
+		};
+		recalculateScalingResource(1, res);
+		trace(res);
+		return res;
+	}
+}
+
+class AttributeLogic {
+	public static function AddOld(attributes:Map<String, Int>, attributeAddition:Map<String, Float>, quantityOfAddition:Int) {
+		for(key => value in attributes){
+			attributes[key] += Std.int(attributeAddition[key]*quantityOfAddition);
+		}
+	}
+	public static function Add(attributes:Map<String, Int>, attributeAddition:Map<String, Float>, quantityOfAddition:Int, result:Map<String, Int>) {
+		for(key => value in attributeAddition){
+			result[key] = attributes[key] + Std.int(attributeAddition[key]*quantityOfAddition);
+		}
+	}
 }
 
 typedef Actor = {
 	var level:Int;
 	var xp:ScalingResource;
 	var attributesBase:Map<String, Int>;
+	var attributesCalculated:Map<String, Int>;
 	var equipment:Array<Equipment>;
 	var equipmentSlots:Array<Int>;
 }
@@ -120,13 +159,15 @@ typedef LevelGrowth = {
 typedef ScalingResource = {
 	var value:Int;
 	var scaling:Scaling;
+	// this is buffered data to avoid recalculation
+	var calculatedMax:Int;
+	var lastUsedBaseAttribute:Int;
 }
 
 typedef Scaling = {
-	var data1:Int;
+	var data1:Float;
 	var minimumIncrement:Int;
-	var type: ScalingType;
-
+	var type:ScalingType;
 }
 
 enum ScalingType {
