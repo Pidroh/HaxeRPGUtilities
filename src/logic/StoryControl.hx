47d69723aba2fd54743b71a1262aea8416b12af1
Pedro <forworksfr@hotmail.com>
2021-10-27 21:37:47 +0900

    - fix wrong order of speaker

--- src/logic/StoryControl.hx
+++ src/logic/StoryControl.hx
@@ -11,12 +11,15 @@ class StoryControlLogic {
 				StoryLogic.StartStory(cutscenes[actionId].title, runtime);
 				view.StartStory();
 			}
+			
 			if (actionId == View.storyAction_AdvanceMessage) {
 				StoryLogic.MessageAdvance(runtime);
 			}
-			if(view.storyDialogActive && runtime.cutscene == null){
+
+			if (view.storyDialogActive && runtime.cutscene == null) {
 				view.HideStory();
 			}
+			
 		}
 	}
 
@@ -24,22 +27,23 @@ class StoryControlLogic {
 	// add all necessary arguments
 	public static function Update(update:Float, runtime:StoryRuntimeData, view:View) {
 		view.StoryButtonAmount(runtime.cutscenes.length);
-		for (i in 0...runtime.cutscenes.length){
+		for (i in 0...runtime.cutscenes.length) {
 			var completed = false;
-			if(runtime.persistence.progressionData[runtime.cutscenes[i].title] != null){
+			if (runtime.persistence.progressionData[runtime.cutscenes[i].title] != null) {
 				completed = runtime.persistence.progressionData[runtime.cutscenes[i].title].timesCompleted > 0;
 			}
-			view.StoryButtonFeed(i, 
-				runtime.cutscenes[i].title, 
-				completed);
+			view.StoryButtonFeed(i, runtime.cutscenes[i].title, completed);
 		}
-			var cutscene = runtime.cutscene;
+		var cutscene = runtime.cutscene;
 		if (cutscene != null) {
 			var m = cutscene.messages[runtime.currentStoryProgression.index];
-			view.LatestMessageUpdate(m.speaker, m.body, runtime.currentStoryProgression.index);
+			view.LatestMessageUpdate(m.body, m.speaker, runtime.currentStoryProgression.index);
 		}
-
-		for (i in 0...runtime.cutscenes.length) {}
+		
+		
+		for (i in 0...runtime.cutscenes.length) {
+			
+		}
 		StoryLogic.Update(runtime);
 	}
 }
