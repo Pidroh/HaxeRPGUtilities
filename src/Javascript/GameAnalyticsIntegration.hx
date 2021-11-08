e2e6eba069503b1d4115d48337e73348bbaa0c57
Pedro <forworksfr@hotmail.com>
2021-11-08 15:29:12 +0900

    - change analytics to dev version after release

--- src/Javascript/GameAnalyticsIntegration.hx
+++ src/Javascript/GameAnalyticsIntegration.hx
@@ -12,7 +12,7 @@ class GameAnalyticsIntegration{
             gameanalytics.GameAnalytics.initialize({0},{1}); 
             
         }
-        ', gameKey, secretKey, "0.7.0dev");
+        ', gameKey, secretKey, "0.8.0dev");
     }
 
     public static function SendDesignEvent(eventName:String, value:Int){
