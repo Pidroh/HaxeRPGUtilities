16a284ac40be4631f8229412a60120b230dd8624
Pedro <forworksfr@hotmail.com>
2021-11-10 09:30:56 +0900

    ## 0.07B

--- src/Javascript/GameAnalyticsIntegration.hx
+++ src/Javascript/GameAnalyticsIntegration.hx
@@ -12,7 +12,7 @@ class GameAnalyticsIntegration{
             gameanalytics.GameAnalytics.initialize({0},{1}); 
             
         }
-        ', gameKey, secretKey, "0.7.2");
+        ', gameKey, secretKey, "0.8.0bdev");
     }
 
     public static function SendDesignEvent(eventName:String, value:Int){
