ee4a07bb038f7344707f1f65596757b623299550
Pedro <forworksfr@hotmail.com>
2021-11-16 22:39:08 +0900

    - add knife and make it highest DPS

--- src/logic/PrototypeItemMaker.hx
+++ src/logic/PrototypeItemMaker.hx
@@ -20,15 +20,16 @@ class PrototypeItemMaker {
 		AddItem("Vest", itemType_Armor, ["LifeMax" => 3, "Defense" => 0.6]);
 		AddItem("Plate", itemType_Armor, ["Defense" => 1]);
 		AddItem("Broad Sword", itemType_Weapon, ["Attack" => 1]);
-		AddItem("Heavy Sword", itemType_Weapon, ["Attack" => 1], ["Attack" => R(115, 115), "Speed" => R(90, 90)]);
-		AddItem("Bastard Sword", itemType_Weapon, ["Attack" => 1], ["Attack" => R(150, 150), "Speed" => R(70, 70)]);
+		AddItem("Heavy Sword", itemType_Weapon, ["Attack" => 1], ["Attack" => R(115, 115), "Speed" => R(80, 80), "Piercing" => R(20,20)]);
+		AddItem("Bastard Sword", itemType_Weapon, ["Attack" => 1], ["Attack" => R(150, 150), "Speed" => R(50, 50), "Piercing" => R(50,50)]);
+        AddItem("Dagger", itemType_Weapon, ["Attack" => 1], ["Attack" => R(70, 70), "Speed" => R(175, 175)]);
 
-		AddMod("of the Brute", "Barbarian's", ["Attack" => R(105, 120)]);
+		AddMod("of the Brute", "Barbarian's", ["Attack" => R(105, 110)]);
 		AddMod("of the Guardian", "Golem's", ["Defense" => R(120, 150)]);
 		AddMod("of the Thief", "Zidane's", ["Speed" => R(115, 130)]);
 		AddMod("of Nature", "Aerith's", ["LifeMax" => R(130, 150)]);
 
-		AddMod("of Rage", "Beserker's", ["Attack" => R(130, 150), "Defense" => R(70, 90)]);
+		AddMod("of Rage", "Beserker's", ["Attack" => R(115, 125), "Defense" => R(70, 90)]);
 
 		// AddItem("Bastard Sword", itemType_Weapon, ["Attack" => 1], ["Attack"=>R(200,280), "Speed"=>R(30, 45)]);
 		// AddItem("Bastard Sword", itemType_Weapon, ["Attack" => 1.1], ["Speed"=>R(30, 40), "Power"=>(50,60)]);
